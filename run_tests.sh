9a78dd31d7275a4d9048029159cc872c48c2d7cf
Florian CASTELAIN <florian.castelain@viacesi.fr>
2023-04-08 12:43:35 +0200

    feat(tests) slighty more friendly test output

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,11 @@
+exit_info() {
+    if [ $? -ne 0 ]; then
+        echo "ERROR - Something wrong happened"
+    else
+        echo "INFO - tests sucessful"
+    fi
+}
+
+trap 'exit_info' EXIT
+
 echo "" | awk -f tests/scripts/test_clean.awk
